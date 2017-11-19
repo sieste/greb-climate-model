70e26822e8f4f3104b67b5cc5ba8e7e862b7ebdd
Stefan Siegert <stefan_siegert@gmx.de>
2017-11-19 12:54:37 +0000

    implemented function read_greb to read input/output fields and used it in analysis files

--- R/analyse_input_fields.R
+++ R/analyse_input_fields.R
@@ -1,56 +1,35 @@
-library(tidyverse)
-library(stringr)
-library(lubridate)
+source('functions.R')
 
-# define lons and lats
-nlon   = 96
-dlon   = 360 / nlon
-lon    = seq(dlon/2, 360-dlon/2, len=nlon)
-nlat   = 48
-dlat   = 180 / nlat
-lat    = seq(-90+dlat/2, 90-dlat/2, len=nlat)
-# 2d fields are saved as arrays in row-major order, hence expand.grid(lon, lat)
-lonlat = expand.grid(lon=lon, lat=lat) %>% as_data_frame
-
-# read topography file and create data frame
-topo = readBin('../input/topography', numeric(), n=nlon*nlat, size=4)
-topo_df = lonlat %>% mutate(topo=topo, type = ifelse(topo<0, 'ocean', 'land'))
+# read topography file (stationary global field)
+topo = read_greb('../input/topography', tstamps=as.Date('1900-01-01'), 
+                 varname='topo', ivar=1, nvar=1)
+topo = topo %>% mutate(type = ifelse(topo<0, 'ocean', 'land'))
 
 # plot land/sea mask
-ggplot(topo_df) + geom_raster(aes(x=lon, y=lat, fill=type))
+ggplot(topo) + geom_raster(aes(x=lon, y=lat, fill=type))
 
 
 # read wind fields (global field, twice daily for one year)
-nstep_yr = 365 * 2 
-ufield = readBin('../input/zonal.wind',      numeric(), n=nlon*nlat*nstep_yr, size=4)
-vfield = readBin('../input/meridional.wind', numeric(), n=nlon*nlat*nstep_yr, size=4)
-time_df = data_frame(time = seq(from = as.POSIXlt('1900-01-01 00:00'), 
-                                to   = as.POSIXlt('1900-12-31 12:00'), 
-                                len  = nstep_yr))
-wind_df = bind_cols(
-  time_df %>% slice(rep(1:n(), each=nlon*nlat)),
-  lonlat  %>% slice(rep(1:n(), nstep_yr)),
-  u = ufield,
-  v = vfield)
+tstamps = seq(from=as.POSIXlt('1900-01-01 00:00'), to=as.POSIXlt('1900-12-31 12:00'), len=2*365)
+ufield  = read_greb(file='../input/zonal.wind', tstamps=tstamps, ivar=1, nvar=1, varname='u')
+vfield  = read_greb(file='../input/meridional.wind', tstamps=tstamps, ivar=1, nvar=1, varname='v')
+wind    = ufield %>% mutate(v = vfield$v)
 
 # plot only the first wind field
 ggplot() + 
-  geom_raster(data=topo_df, mapping=aes(x=lon, y=lat, fill=type), show.legend=FALSE) +
-  geom_segment(data=filter(wind_df, yday(time)==1), 
+  geom_raster(data=topo, mapping=aes(x=lon, y=lat, fill=type), show.legend=FALSE) +
+  geom_segment(data=filter(wind, yday(time)==1), 
                mapping=aes(x=lon-u/4, y=lat-v/4, xend=lon+u/4, yend=lat+v/4), 
                arrow=arrow(length=unit(0.1,'cm')))
 
-# calculate monthly averages per grid point
-wind_monavg_df = wind_df %>% 
-  mutate(month=month(time)) %>%
-  group_by(month, lon, lat) %>% 
-  summarise(u=mean(u), v=mean(v))
+# calculate monthly wind speeds per grid point
+wind_monavg = 
+  wind %>% mutate(month=month(time)) %>%
+  group_by(month, lon, lat) %>% summarise(u=mean(u), v=mean(v))
 
 # plot monthly average wind fields
-ggplot(wind_monavg_df) + facet_wrap(~month, ncol=3) +
-  geom_raster(data=topo_df, mapping=aes(x=lon, y=lat, fill=type), show.legend=FALSE) +
-  geom_segment(data=wind_monavg_df, 
-               mapping=aes(x=lon-u/2, y=lat-v/2, xend=lon+u/2, yend=lat+v/2))
-
+ggplot(wind_monavg) + facet_wrap(~month, ncol=3) +
+  geom_raster(data=topo, mapping=aes(x=lon, y=lat, fill=type), show.legend=FALSE) +
+  geom_segment(data=wind_monavg, mapping=aes(x=lon-u/2, y=lat-v/2, xend=lon+u/2, yend=lat+v/2))
 
 
---
+++ R/functions.R
@@ -0,0 +1,72 @@
+library(tidyverse)
+library(stringr)
+library(lubridate)
+library(rnaturalearth)
+
+
+#' Read a variable from greb model output
+#'
+#' @param file filename
+#' @param tstamps time stamps of the outputs
+#' @param varname variable name
+#' @param ivar variable index if there are several variables
+#' @param nvar number of variables in output file
+#' @param nlon number of longitudes
+#' @param nlat number of latitudes
+#' @param nbyte number of bytes per datum
+#' @return a tidy data_frame with columns time, lon, lat, and the requested variable
+#'
+#' @details
+#' 2d fields are saved as arrays in row-major order, so we use
+#' `expand.grid(lon, lat)` instead of `(lat, lon)`.
+#'
+#' In the default greb output, 50 years of monthly mean fields for 5 variables
+#' (Tsurf, Tair, Tocn, qatm, albedo) are written in order [year, month,
+#' variable, lon, lat]. Each saved number uses 4 bytes of memory. So output
+#' fields are written in chunks of size (4*n_grid) bytes. For example, albedo is
+#' the 5th variable, so the first albedo field (for year 1, month 1) starts at
+#' position (n_grid * 4), the second albedo field (year 1, month 2) starts at
+#' (n_grid * 4 + 5 * n_grid * 4), and so on. So we jump 4 * 4 * n_grid bytes
+#' using seek(), and then read 4 * n_grid bytes using readBin(). The data is
+#' iteratively appended to a list which avoids making deep copies, as opposed to
+#' appending to a vector.
+# 
+read_greb = function(file, tstamps, 
+                     varname=str_c('variable_',ivar), 
+                     ivar=1L, nvar=5L, 
+                     nlon=96, nlat=48, nbyte=4) {
+
+  stopifnot(file.exists(file))
+  stopifnot(file.size(file) == nlon * nlat * nvar * nbyte * length(tstamps))
+  # latitude/longitude grid (longitude varies fastest)
+  ngrid = nlon * nlat
+  dlon   = 360 / nlon
+  dlat   = 180 / nlat
+  lon    = seq(dlon/2, 360-dlon/2, len=nlon)
+  lat    = seq(-90+dlat/2, 90-dlat/2, len=nlat)
+  lonlat = expand.grid(lon=lon, lat=lat) %>% as_data_frame
+
+  ntime = length(tstamps)
+
+  # read requested data from file
+  con  = file(file, open='rb')
+  out  = list()
+  nout = 0
+  for (ii in seq_len(ntime)) {
+    seek(con, where = nbyte * ngrid * ((ii-1) * nvar + (ivar - 1)))
+    out[nout + 1:ngrid] = readBin(con=con, what=numeric(), n=ngrid, size=nbyte)
+    nout = nout + ngrid
+  }
+  close(con)
+  out = list(unlist(out)) %>% setNames(varname) %>% as_data_frame
+
+  # generate data frame with time, lon, lat, and value
+  out_df = bind_cols(
+    data_frame(time = tstamps) %>% slice(rep(1:ntime, each=ngrid)),
+    lonlat                     %>% slice(rep(1:ngrid, ntime)),
+    out)
+
+  return(out_df) 
+} 
+
+
